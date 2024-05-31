8602bdd539bc3bd13a6d4196d814217bb34a2b2a
Emerson Soares da Silva <esds.emerson1993@gmail.com>
2024-05-31 16:24:34 -0300

    projeto conta banco.

--- conta-banco/src/contaTerminal.java
+++ conta-banco/src/contaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class contaTerminal {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = sc.nextLine();
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = sc.nextInt();
+        sc.nextLine();  // Consumir a nova linha
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = sc.nextLine();
+
+        System.out.println("Por favor, digite o saldo da Conta:");
+        double saldo = sc.nextDouble();
+
+        // Exibindo a mensagem com os dados inseridos
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
